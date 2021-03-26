3565f42dad3690f19b389190ac1073e6ed0aa665
Lagnajeet Das <lagnajeetdas@gmail.com>
2021-03-26 11:25:51 +0100

    optimization of option chain put sells and spreads queries for faster UI loading

--- app/controllers/options_controller.rb
+++ app/controllers/options_controller.rb
@@ -24,9 +24,35 @@ class OptionsController < ApplicationController
       #p @optionchains
 
 
-      @high_open_interest = Optionhighopeninterest.all
-      @best_rr_options = Optionscenario.all
-      @best_sp_options = Optionputsell.all
+      #@high_open_interest = Optionhighopeninterest.all
+    
+      @best_sp_options = Optionputsell.find_by_sql(
+              "SELECT
+                 *
+               FROM optionputsells
+               WHERE optionputsells.premiumratio>0.01 
+               and (optionputsells.strike / optionputsells.quote) <= 0.9
+               ORDER BY optionputsells.premiumratio DESC, 
+               (optionputsells.strike / optionputsells.quote) ASC
+               LIMIT 2000
+               "
+               )
+
+      @best_rr_options = Optionscenario.find_by_sql(
+              "SELECT
+                 *
+               FROM optionscenarios
+               WHERE optionscenarios.rr_ratio>1 
+               and optionscenarios.risk<500 
+               and optionscenarios.risk>25 
+               and optionscenarios.reward>50 
+               and optionscenarios.perc_change<20
+               ORDER BY optionscenarios.rr_ratio DESC, optionscenarios.perc_change ASC 
+               LIMIT 1000
+               "
+               )
+
+
       
     end
 
